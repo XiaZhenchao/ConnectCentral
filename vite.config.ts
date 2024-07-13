b82b25d11e55d6e447b557872f2d5abbf3ea077a
Xia Zhenchao <xiazhenchao96@gmail.com>
2024-07-13 01:15:01 -0400

    basic setting for docker

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,10 @@ export default defineConfig({
    port: 8080,
    strictPort: true,
   },
-  // server: { //An object for the Server options
-  //   port: 8080, //Specify server port. Note if the port is already being used, Vite will automatically try the next available port so this may not be the actual port the server ends up listening on.
-  //   strictPort: true, // Specify server port. Note if the port is already being used, Vite will automatically try the next available port so this may not be the actual port the server ends up listening on.
-  //   host: true, // Specify which IP addresses the server should listen on. Set this to 0.0.0.0 or true to listen to all addresses, including LAN and public addresses.
-  //   origin: "http://0.0.0.0:8080", // Defines the origin of the generated asset URLs during development.
-  //  },
+  server: { //An object for the Server options
+    port: 8080, //Specify server port. Note if the port is already being used, Vite will automatically try the next available port so this may not be the actual port the server ends up listening on.
+    strictPort: true, // Specify server port. Note if the port is already being used, Vite will automatically try the next available port so this may not be the actual port the server ends up listening on.
+    host: true, // Specify which IP addresses the server should listen on. Set this to 0.0.0.0 or true to listen to all addresses, including LAN and public addresses.
+    origin: "http://0.0.0.0:8080", // Defines the origin of the generated asset URLs during development.
+   },
 })
